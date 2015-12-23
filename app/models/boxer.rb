81e48e6757fcddc4b64540d28a8899ba102579af
Anthony Wu <anthonylongwu19@gmail.com>
2015-12-22 21:30:43 -0800

    made changes to controller, http token

--- app/models/boxer.rb
+++ app/models/boxer.rb
@@ -12,10 +12,13 @@ attr_accessor :id, :first_name, :last_name, :weight_class, :current_record, :tit
     @home_town = hash["home_town"]
   end
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 
   def self.all
     boxers = []
-    boxer_hashes = Unirest.get("http://localhost:3000/api/v1/boxers.json").body
+    boxer_hashes = Unirest.get("#{ENV['API_BASE_URL']}/boxers.json", headers: {"X-User-Email": "test@test.com", "Authorization": "Token token=ABC123"}).body
     boxer_hashes.each do |boxer_hash|
      boxers << Boxer.new(boxer_hash)
     end
@@ -23,23 +26,23 @@ attr_accessor :id, :first_name, :last_name, :weight_class, :current_record, :tit
   end
 
   def self.find(id)
-    boxer_hash = Unirest.get("http://localhost:3000/api/v1/boxers.json").body
+    boxer_hash = Unirest.get("#{ENV['API_BASE_URL']}/boxers/#{id.json"}").body
     Boxer.new(boxer_hash)
   end
 
   def self.create(attributes)
-    boxer_hash = Unirest.post("http://localhost:3000/api/v1/boxers.json", headers: {"Accept" => "application/json"}, parameters: attributes).body
+    boxer_hash = Unirest.post("#{ENV['API_BASE_URL']}/boxers.json", headers: {"Accept" => "application/json"}, parameters: attributes).body
     Boxer.new(boxer_hash)
   end
   
   def update(attributes)
-    boxer_hash = Unirest.patch("http://localhost:3000/api/v1/boxers.json/#{params[:id]}.json", headers: {
+    boxer_hash = Unirest.patch("#{ENV['API_BASE_URL']}/boxers.json/#{params[:id]}.json", headers: {
     "Accept" => "application/json"}, parameters: attributes).body
     Boxer.new(boxer_hash)
   end
 
   def destroy
-    Unirest.delete("http://localhost:3000/api/v1/boxers.json/#{id}.json").body
+    Unirest.delete("#{ENV['API_BASE_URL']}/boxers.json/#{id}.json").body
     redirect_to '/boxers'
   end
 
